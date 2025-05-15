86f0687cdb27a06913e0ddc5ace4bb0198416344
RoselineDC <roseline.danga@gmail.com>
2025-05-15 14:39:48 +0200

    modified by Roseline on 2025-05-15 14:39:48

--- components/productCard.tsx
+++ components/productCard.tsx
@@ -21,7 +21,7 @@ const ProductCard = ({ product }: Props) => {
 
       <div className="flex flex-col gap-3">
         <h3 className="product-title">{product.title}</h3>
-        <div className="flex-stars">
+        <div className="flex just-stars">
       </div>
     </Link>
   );
