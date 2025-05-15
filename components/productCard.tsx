7316cc75d6ab3fd99823bb3abc26876c6be57fe4
RoselineDC <roseline.danga@gmail.com>
2025-05-15 14:33:53 +0200

    modified by Roseline on 2025-05-15 14:33:53

--- components/productCard.tsx
+++ components/productCard.tsx
@@ -11,7 +11,7 @@ const ProductCard = ({ product }: Props) => {
     <Link href={`/products/${product._id}`} className="product-card">
       <img src={product.image}
        alt={product.title}
-       
+       NAME
         />
     </Link>
   );
