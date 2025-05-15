13914d9e900d6a32c7e85f14b9ec92d40f44d735
RoselineDC <roseline.danga@gmail.com>
2025-05-15 20:42:10 +0200

    modified by Roseline on 2025-05-15 20:42:09

--- components/Seachbar.tsx
+++ components/Seachbar.tsx
@@ -14,7 +14,7 @@ const isValidAmazonProductUrl = (url: string) => {
             hostname.includes('amazon.co.za') ||
             hostname.includes('amazon') ||
             hostname.endsWith('amazon') ||
-            hostname.ends
+            hostname.endsWith
         ) {
             return true;
 
