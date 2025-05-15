5a3d2b2afd9180fdde7104c84eef18bbacb4f476
RoselineDC <roseline.danga@gmail.com>
2025-05-15 13:54:25 +0200

    modified by Roseline on 2025-05-15 13:54:24

--- lib/action/index.ts
+++ lib/action/index.ts
@@ -63,6 +63,7 @@ function getAveragePrice(updatedPriceHistory: any): number {
 }
 export async function getProductById(productId: string) {
     try{
+        conn
 
     } catch (error) {
         
