98e14e5f7a4430cd28e1561c6064e6caada6a772
RoselineDC <roseline.danga@gmail.com>
2025-05-15 14:22:28 +0200

    modified by Roseline on 2025-05-15 14:22:28

--- app/page.tsx
+++ app/page.tsx
@@ -47,7 +47,7 @@ const Home = async () => {
         </h2>
         <div className='flex  flex-wrap gap-x-8 gap-y-16'>
           {allProducts?.map((product) => (
-            <div>{product}</div>
+            <div>{product.title}</div>
           ))}
           
             
