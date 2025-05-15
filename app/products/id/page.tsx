64e3060b2d30dfc2b3863f37202e2177cd95fd75
RoselineDC <roseline.danga@gmail.com>
2025-05-15 14:08:00 +0200

    modified by Roseline on 2025-05-15 14:08:00

---
+++ app/products/id/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ProductDetails = () => {
+  return (
+	<div>
+	  {/* Product details content goes here */}
+	  <h1>Product Details</h1>
+	</div>
+  );
+};
+
+export default ProductDetails
