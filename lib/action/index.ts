bb9e7bed1fe6358309036c63c212d1d974a3b5a2
RoselineDC <roseline.danga@gmail.com>
2025-05-15 13:58:12 +0200

    modified by Roseline on 2025-05-15 13:58:11

--- lib/action/index.ts
+++ lib/action/index.ts
@@ -65,7 +65,9 @@ export async function getProductById(productId: string) {
     try{
         connectToDB();
         // fetch product
-        const product = await Product.findOne({})
+        const product = await Product.findOne({
+            _id: productId
+        });
 
     } catch (error) {
         
