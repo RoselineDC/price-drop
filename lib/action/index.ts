b1ba9f5e6dc5bff60f21aa722dfe84cfe7f17118
RoselineDC <roseline.danga@gmail.com>
2025-05-15 13:53:07 +0200

    modified by Roseline on 2025-05-15 13:53:07

--- lib/action/index.ts
+++ lib/action/index.ts
@@ -61,6 +61,6 @@ export async function scrapeAndStoreProducts(productUrl: string) {
 function getAveragePrice(updatedPriceHistory: any): number {
     throw new Error("Function not implemented.");
 }
-export async function name(params:type) {
+export async function getP(params:type) {
     
 }
