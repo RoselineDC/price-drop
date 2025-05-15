cf40ef5f131479a86a5d508c632dcfa765743b7e
RoselineDC <roseline.danga@gmail.com>
2025-05-15 14:32:03 +0200

    modified by Roseline on 2025-05-15 14:32:03

--- components/productCard.tsx
+++ components/productCard.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const ProductCard = ({product }: Props) => {
   return (
-    <Link href={`/products/${product._id}`}>productCard</Link>
+    <Link href={`/products/${product._id}`}>P</Link>
   )
 }
 
