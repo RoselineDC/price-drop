336b6f738445c669be226ba2b10edfbfa4b4429e
RoselineDC <roseline.danga@gmail.com>
2025-05-15 13:33:25 +0200

    modified by Roseline on 2025-05-15 13:33:25

--- lib/action/index.ts
+++ lib/action/index.ts
@@ -60,7 +60,7 @@ export async function scrapeAndStoreProducts(productUrl: string) {
                 upsert: true,
             }
         );
-        revalidatePath(`/products/${newProduct._id`);});
+        revalidatePath(`/products/${newProduct._id`););
                       
 
         
