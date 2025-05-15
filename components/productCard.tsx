1a02f17dc27a6370d43f29136a9c377b3e9b2f79
RoselineDC <roseline.danga@gmail.com>
2025-05-15 14:42:23 +0200

    modified by Roseline on 2025-05-15 14:42:22

--- components/productCard.tsx
+++ components/productCard.tsx
@@ -27,7 +27,7 @@ const ProductCard = ({ product }: Props) => {
             </p>
             <P>
                 <span>
-                    
+                    {}
                 </span>
             </P>
         </div>
