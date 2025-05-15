b09a2cdf1e78c51c2c964d2f82ff121b2c9c01a9
RoselineDC <roseline.danga@gmail.com>
2025-05-15 14:13:03 +0200

    modified by Roseline on 2025-05-15 14:13:03

--- app/page.tsx
+++ app/page.tsx
@@ -46,7 +46,7 @@ const Home = async () => {
           Trending in Tech
         </h2>
         <div className='flex  flex-wrap gap-x-8 gap-y-16'>
-          {['Apple Iphone 14 Pro', 'Samsung Galaxy S23 Ultra', 'Google Pixel 7 Pro'].map
+          {.map
           ((product) => (
             <div>{product}</div>
           ))}
