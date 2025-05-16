02b34eaad1e332c1031e67c2f23607031a1d0fc4
RoselineDC <roseline.danga@gmail.com>
2025-05-16 14:15:45 +0200

    modified by Roseline on 2025-05-16 14:15:44

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -31,8 +31,8 @@ const ProductDetails = async  ( {params: { id }}: Props) => {
           <p className=' text-[28] text-secondary font-semibold'>
             {product.title}
             </p>
-            <Link href={product.title}     
-            
+            <Link href={product.title} 
+                        
             />
           </div>
 
