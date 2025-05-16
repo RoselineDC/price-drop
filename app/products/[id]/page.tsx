7a5fe347020e486d4d8a6fd9bb80c737a46904c1
RoselineDC <roseline.danga@gmail.com>
2025-05-16 14:16:41 +0200

    modified by Roseline on 2025-05-16 14:16:41

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -33,7 +33,8 @@ const ProductDetails = async  ( {params: { id }}: Props) => {
             </p>
             <Link href={product.title} 
             target='_blank'
-            className=' text-base text-black opacity-50'            
+            className=' text-base text-black opacity-50'
+                        
             />
           </div>
 
